9ee032e6df478c5958e62c2c4b808c66c1a72250
Mett686 <99293704+Mett686@users.noreply.github.com>
2023-11-27 14:03:44 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,55 +1,66 @@
-//
 const express = require('express');
 const app = express();
 const fs = require('fs');
 
 //Settings
-const port = 3000;
+const PORT = 3000;
+const DATADIR = __dirname;
+//-----spaghetti-beyond-------
 
-function logData(data, PATH) {
-
-    var FILE = `${PATH}/weather-${data.time.slice(0, 7)}.csv`;
+function logData(data) {
+    var FILE = DATADIR + `/data/${data.time.slice(0, 7)}.csv`;
     const line = `${data.time},${data.temperature},${data.pressure},${data.humidity},${data.windSpeed},${data.windDirection},${data.radiation}\r\n`;
     
     try {
-      fs.appendFileSync( FILE, line);
-    } catch (err) {
-      console.error(err);
-    };
+        fs.appendFileSync( FILE, line);
+      } catch (err) {
+        console.error(err);
+      };
 };
 
-//This is here for the receiver to see req.body
+//csv to array (file is the year and month '2020-12')
+function getArray(file) {
+    fs.open( DATADIR + `/data/${file}`,'r' , function (f) {
+        conso
+    })
+};
+
+//Express
 app.use(express.json());
 
-//Get request for the main page
 app.get('/', (req, res) => {
     console.log(`${req.method} ${req.object} HTTP/${req.httpVersion}`);
-
-    res.sendFile(__dirname + 'index.html');
+    //work in progress
+    res.sendFile(DATADIR + '/website/Home.html');
 });
 
-//Json file request
 app.get('/data', (req, res) => {
     console.log(`${req.method} ${req.path} HTTP/${req.httpVersion}`);
 
-    res.send( data );
+    res.json( data );
+});
+//finish this!!
+app.get('/array', (req, res) => {
+    console.log(`${req.method} ${req.path} HTTP/${req.httpVersion}`);
+
+    
+
+    res.json();
 });
 
-//Data receiver
 app.put('/data', (req, res) => {
     console.log(`${req.method} ${req.path} HTTP/${req.httpVersion}`);
     
     data = req.body;
-    data.time = new Date().toISOString();
+    data.time = new Date().toISOString().slice(0,17);
 
     console.log(`Received data ${data.time.replace('T' ,' ').slice(0,-5)}`);
-    logData(data, __dirname + '/data')
+    logData(data)
     //console.log(data.toString())
 
     res.send(req.statusMessage);
 });
 
-//Listener
-app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}`);
 });
