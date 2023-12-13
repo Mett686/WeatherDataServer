66c49988cc19d93403bd73ce3ca940064ab51e5b
Mett686 <99293704+Mett686@users.noreply.github.com>
2023-12-13 22:11:50 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const fs = require('fs');
 
 //Settings
 const PORT = 3000;
-const DATADIR = __dirname;
+const DIR = __dirname;
 //-----spaghetti-beyond-------
 
 function logData(data) {
-    var FILE = DATADIR + `/data/${data.time.slice(0, 7)}.csv`;
+    var FILE = DIR + `/data/${data.time.slice(0, 7)}.csv`;
     const line = `${data.time},${data.temperature},${data.pressure},${data.humidity},${data.windSpeed},${data.windDirection},${data.radiation}\r\n`;
     
     try {
@@ -18,20 +18,13 @@ function logData(data) {
       };
 };
 
-//csv to array (file is the year and month '2020-12')
-function getArray(file) {
-    fs.open( DATADIR + `/data/${file}`,'r' , function (f) {
-        conso
-    })
-};
-
 //Express
 app.use(express.json());
 
 app.get('/', (req, res) => {
     console.log(`${req.method} ${req.object} HTTP/${req.httpVersion}`);
     //work in progress
-    res.sendFile(DATADIR + '/website/Home.html');
+    res.sendFile(DIR + '/website/Home.html');
 });
 
 app.get('/data', (req, res) => {
@@ -40,12 +33,41 @@ app.get('/data', (req, res) => {
     res.json( data );
 });
 //finish this!!
-app.get('/array', (req, res) => {
-    console.log(`${req.method} ${req.path} HTTP/${req.httpVersion}`);
+app.get('/array/:startTime/:stopTime', (req, res) => {
+  const startTime = req.params.startTime;
+  const stopTime = req.params.stopTime;
 
-    
+  const csvFile = `${(req.params.startTime).slice(0, 7)}.csv`
 
-    res.json();
+  console.log('array request')
+
+  try {
+    const csv = fs.readFileSync(`${DIR}/data/${csvFile}`, { encoding: 'utf8' });
+
+    const csvArray = csv.split(`\r\n`); // Split the CSV string into an array of lines
+
+    const filteredCsvArray = csvArray
+      .filter((line, index) => {
+        if (index === 0) {
+          // Include the header row in the result
+          return true;
+        }
+
+        const values = line.split(','); // Assuming values are separated by commas
+        const timeValue = new Date(values[0]); // Assuming time is the first element in each row
+
+        const startTimeDate = new Date(startTime);
+        const stopTimeDate = new Date(stopTime);
+
+        return !isNaN(timeValue) && timeValue >= startTimeDate && timeValue <= stopTimeDate;
+      });
+
+    res.json(filteredCsvArray);
+
+  } catch (err) {
+    console.error('Error reading the file:', err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.put('/data', (req, res) => {
@@ -56,7 +78,6 @@ app.put('/data', (req, res) => {
 
     console.log(`Received data ${data.time.replace('T' ,' ').slice(0,-5)}`);
     logData(data)
-    //console.log(data.toString())
 
     res.send(req.statusMessage);
 });
