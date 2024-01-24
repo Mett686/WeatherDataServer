e6692daee9b222191e4b27a5f678ca9634dec975
Mett686 <knotekmartin13@gmail.com>
2024-01-24 15:25:10 +0100

    Server.js and web udpate server now soving 10 min averages and download was added  On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   index.html
    	modified:   server.js
    	modified:   starter.bat
    
     Untracked files:
    	test.js
    
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
    
     Date:      Wed Jan 24 15:25:10 2024 +0100
    
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   index.html
    	modified:   server.js
    	modified:   starter.bat

--- server.js
+++ server.js
@@ -7,19 +7,103 @@ const fs = require('fs');
 // Settings
 const PORT = 3000;
 const DIR = __dirname;
+var dataPath = DIR + '/data/';
 
 //-----spaghetti-beyond-------
+let logDataN = 0;
+let dataSum = {
+    temperature: 0,
+    pressure: 0,
+    humidity: 0,
+    windSpeed: 0,
+    radiation: 0,
+    windDirection: {
+        sin: 0,
+        cos: 0
+    }
+};
+
+function avg(sum) {
+  return sum / logDataN
+}
+
+function DegToRad(degrees) {
+  return degrees * (Math.PI /180)
+}
 
 function logData(data) {
-    var FILE = DIR + `/data/${data.time.slice(0, 4)}.csv`;
-    const line = `${data.time.slice(0,-5)}Z,${data.temperature},${data.pressure},${data.humidity},${data.windSpeed},${data.windDirection},${data.radiation}\r\n`;
-    
-    try {
-        fs.appendFileSync( FILE, line);
-      } catch (err) {
-        console.error(err);
-      };
-};
+  // Here we sum the data for dataWrite to average them
+  dataSum.temperature += data.temperature;
+  dataSum.pressure += data.pressure;
+  dataSum.humidity += data.humidity;
+  dataSum.windSpeed += data.windSpeed;
+  dataSum.radiation += data.radiation;
+  dataSum.windDirection.sin += Math.sin(DegToRad(data.windDirection)) //This is here for the possibility that the windDirection values are around 0/360 which would result in 180 average, which is exactly opposite of the actual value
+  dataSum.windDirection.cos += Math.cos(DegToRad(data.windDirection))
+  logDataN +=1
+}
+
+function dataWrite() {
+  let dataAverage = {
+      temperature: 0,
+      pressure: 0,
+      humidity: 0,
+      windSpeed: 0,
+      radiation: 0,
+      windDirection: {
+          degrees: 0,
+          sin: 0,
+          cos: 0
+  }}
+
+  // write the averaging algorithm
+  dataAverage.temperature = avg(dataSum.temperature);
+  dataAverage.pressure = avg(dataSum.pressure);
+  dataAverage.humidity = avg(dataSum.humidity);
+  dataAverage.windSpeed = avg(dataSum.windSpeed);
+  dataAverage.radiation = avg(dataSum.radiation);
+  dataAverage.windDirection.sin = avg(dataSum.windDirection.sin);
+  dataAverage.windDirection.cos = avg(dataSum.windDirection.cos);
+
+  const degrees = Math.atan2(dataAverage.windDirection.sin, dataAverage.windDirection.cos) * (180 / Math.PI);
+  dataAverage.windDirection.degrees = ( degrees + 360 ) % 360;
+
+  dataString = `${new Date().toISOString().slice(0,13)}, ${dataAverage.temperature}, ${dataAverage.pressure}, ${dataAverage.humidity}, ${dataAverage.windSpeed}, ${dataAverage.windDirection.degrees}, ${dataAverage.radiation}\r\n`
+
+  try {
+      fs.appendFileSync(dataPath + new Date().toISOString().slice(0, 4) + '.csv', dataString);
+      dataString = '';
+      console.log(`${new Date().toISOString().replace('T', ' ').slice(0,-1)} Written data`);
+  } catch (err) {
+      console.error(err);
+  }
+}
+
+function calculateNextWriteTime() {
+  const now = new Date();
+  const minutes = now.getMinutes();
+  const nextWriteMinutes = Math.ceil(minutes / 10) * 10 + 10; // This rounds up to nearest 10 minutes and adds 10, otherwise it would spam if the nearest ten minutes are now
+  
+  if (minutes >= 50) {
+    nextWriteMinutes = 60;
+  }
+  
+  const nextWriteTime = new Date(now);
+  nextWriteTime.setMinutes(nextWriteMinutes, 0, 0);
+
+  const timeUntilNextWrite = nextWriteTime - now;
+
+  return timeUntilNextWrite;
+}
+
+function scheduleNextWrite() { 
+  const timeUntilNextWrite = calculateNextWriteTime();
+
+  setTimeout(() => {
+      dataWrite();
+      scheduleNextWrite();
+  }, timeUntilNextWrite);
+}
 
 // Express
 app.use(cors());
@@ -27,7 +111,7 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     console.log(`${req.method} ${req.object} HTTP/${req.httpVersion}`);
-    // work in progress
+
     res.sendFile(DIR + '/index.html');
 });
 
@@ -37,16 +121,22 @@ app.get('/data', (req, res) => {
     res.json( data );
 });
 
-app.get('/array/:startTime/:stopTime', (req, res) => {
+app.get('/data/:file', (req, res) => {
+  console.log(`${req.method} ${req.path} HTTP/${req.httpVersion}`);
+
+  res.download( dataPath + req.params.file );
+});
+
+app.get('/array/:startTime/:stopTime', (req, res) => { //Technically useless now
   const startTime = new Date(req.params.startTime);
   const stopTime = new Date(req.params.stopTime);
 
   const csvFile = `${startTime.toISOString().slice(0, 4)}.csv`;
 
-  console.log('array request');
+  console.log(`${new Date().toISOString().replace('T' ,' ').slice(0,-1)} Received array request`);
 
   try {
-    const csv = fs.readFileSync(`${DIR}/data/${csvFile}`, { encoding: 'utf8' });
+    const csv = fs.readFileSync( dataPath + csvFile , { encoding: 'utf8' });
 
     const csvArray = csv.split(`\r\n`); // Split the CSV string into an array of lines
 
@@ -79,11 +169,15 @@ app.put('/data', (req, res) => {
     data = req.body;
     data.time = new Date().toISOString();
 
-    console.log(`Received data ${data.time.replace('T' ,' ').slice(0,-5)}`);
+    console.log(`${data.time.replace('T' ,' ').slice(0,-1)} Received data`);
     logData(data)
 
     res.send(req.statusMessage);
 });
+
+// This starts the app
+console.log(`${new Date().toISOString().replace('T', ' ').slice(0,-1)} Written data`)
+scheduleNextWrite();
 
 app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}`);
